db879112359f7d96fdfb86526bab5549d50543e7
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-02 22:02:33 -0600

    ·se agregaron las funcion con la db para hacer el crud

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -1,3 +1,10 @@
+/**
+ * recursos:
+ * https://github.com/uokesita/the-little-mongodb-book/blob/master/es/mongodb.markdown
+ * https://docs.mongodb.com/manual/crud/
+ * https://docs.mongodb.com/manual/reference/method/js-collection/
+ * Video 27 platzi
+ */
 const { MongoClient, Objectid } = require('mongodb');
 const { config } = require('../config');
 
@@ -8,18 +15,18 @@ const DB_NAME = config.dbName;
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${DB_NAME}/test?retryWrites=true&w=majority`;
 
 class MongoLib {
-    constructor(){
+    constructor() {
         this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
         this.dbName = DB_NAME;
     }
 
     connect() {
-        if (!MongoLib.connection){
-            MongoLib.connection = new Promise( (resolve, reject) => {
-                this.client.connect( err => {
-                    if(err)
+        if (!MongoLib.connection) {
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect(err => {
+                    if (err)
                         reject(err);
-                    
+
                     console.log('Connected succesfully to mongo db')
                     resolve(this.client.db(this.dbName));
                 })
@@ -27,4 +34,32 @@ class MongoLib {
         }
         return MongoLib.connection;
     }
-})
+
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db.collection(collection).find(query).toArray();
+        });
+    }
+    get(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).findOne({ _id: Objectid(id) }).toArray();
+        })
+    }
+    create(collection, data) {
+        return this.connect().then(db => {
+            return db.collection(collection).insertOne(data);
+        }).then(result => result.insertedId);
+    }
+    update(collection, id, data) {
+        return this.connect().then(db => {
+            return db.collection(collection).updateOne({ _id: Objectid(id) }, { $set: data }, { upsert: true });
+        }).then(result => result.upsertId || id);
+    }
+    delete(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).deleteOne({ _id: Objectid(id) });
+        }).then(() => id)
+    }
+});
+
+module.exports = MongoLib;
